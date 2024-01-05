5e1a272685e1fd2ff11694364fbdfda3120e46bd
arianrdcas <arianrdcas@gmail.com>
2024-01-05 16:42:03 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import { BiEdit } from "react-icons/bi";
+import { BiTrash } from "react-icons/bi";
+
+
+let usuarios = [
+  ["Arian Rodriguez", 35, "06/12/1988"],
+  ["Yeni Delgado", 38, "27/01/1985"],
+  ["Arian Rodriguez", 35, "06/12/1988"],
+  ["Arian Rodriguez", 35, "06/12/1988"],
+];
 
 function App() {
+
+const enviando = (e) => {
+  e.preventDefault();
+  console.log("Enviando");
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app-usuarios">
+        <h1>Registro usuarios</h1>
+        <div className="contenedor-principal">
+          <form className="contenedor-formulario">
+            <input className="input" placeholder="Introduce tu nombre"></input>
+            <input className="input" placeholder="Introduce tu edad"></input>
+            <input
+              className="input"
+              placeholder="Introduce tu fecha de nacimiento"
+            ></input>
+            <div className="contenedor-boton">
+              <button className="boton-agregar-usuario" onClick={enviando}>
+                Agregar
+              </button>
+            </div>
+          </form>
+          <table>
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Edad</th>
+                <th>Fecha de Nacimiento</th>
+                <th>Editar / Eliminar</th>
+              </tr>
+            </thead>
+            <tbody>
+              {usuarios.map((usuario, index) => (
+                <tr key={index}>
+                  <td>{usuario[0]}</td>
+                  <td>{usuario[1]}</td>
+                  <td>{usuario[2]}</td>
+                  <td className="editar-eliminar">
+                    <button className="boton-eliminar-editar">
+                      <BiEdit />
+                    </button>
+                    <button className="boton-eliminar-editar">
+                      <BiTrash />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   );
 }
 
