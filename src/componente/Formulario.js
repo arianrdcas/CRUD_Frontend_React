9978cb1302f4c01603f2f8452a81761d08864207
arianrdcas <arianrdcas@gmail.com>
2024-01-22 20:46:15 -0300

    agregando CSS Grid

--- src/componente/Formulario.js
+++ src/componente/Formulario.js
@@ -1,50 +1,47 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "../estilos-componentes/Formulario.css";
-import {UserContext} from "../AppContext/UserContext.js"
+import { UserContext } from "../AppContext/UserContext.js";
 
 const Formulario = () => {
-
-const {
-  enviando,
-  nombre,
-  edad,
-  fechadenac,
-  setNombre,
-  setEdad,
-  setFechadenac,
-  editingUserId,
-} = useContext(UserContext);
-
+  const {
+    enviando,
+    nombre,
+    edad,
+    fechadenac,
+    setNombre,
+    setEdad,
+    setFechadenac,
+    editingUserId,
+  } = useContext(UserContext);
 
   return (
-      <form className="contenedor-formulario" onSubmit={enviando}>
-        <input
-          className="input"
-          placeholder="Introduce tu nombre"
-          name="nombre"
-          value={nombre}
-          onChange={(e) => setNombre(e.target.value)}
-        ></input>
-        <input
-          className="input"
-          placeholder="Introduce tu edad"
-          name="edad"
-          value={edad}
-          onChange={(e) => setEdad(e.target.value)}
-        ></input>
-        <input
-          className="input"
-          placeholder="Introduce tu fecha de nacimiento"
-          name="fechadenac"
-          value={fechadenac}
-          onChange={(e) => setFechadenac(e.target.value)}
-        ></input>
-        <div className="contenedor-boton">
-          <button className="boton-agregar-usuario">
-            {editingUserId !== null ? "Actualizar" : "Agregar"}
-          </button>
-        </div>
-      </form>
+    <form className="contenedor-formulario" onSubmit={enviando}>
+      <input
+        className="input"
+        placeholder="Introduce tu nombre"
+        name="nombre"
+        value={nombre}
+        onChange={(e) => setNombre(e.target.value)}
+      ></input>
+      <input
+        className="input"
+        placeholder="Introduce tu edad"
+        name="edad"
+        value={edad}
+        onChange={(e) => setEdad(e.target.value)}
+      ></input>
+      <input
+        className="input"
+        placeholder="Introduce tu fecha de nacimiento"
+        name="fechadenac"
+        value={fechadenac}
+        onChange={(e) => setFechadenac(e.target.value)}
+      ></input>
+
+      <button className="boton-agregar-usuario">
+        {editingUserId !== null ? "Actualizar" : "Agregar"}
+      </button>
+    </form>
   );
 };
 
